057a90ba461a755c074e35575f06c225916fa8dc
pan <77523808+panzuki@users.noreply.github.com>
2025-11-29 17:17:21 +0900

    Create TabBar.vue

---
+++ src/components/TabBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-around bg-gray-100 p-2 border-t">
+    <button @click="$emit('update:tab', 'shelfCreate')">棚作成</button>
+    <button @click="$emit('update:tab', 'delivery')">納品</button>
+    <button @click="$emit('update:tab', 'shelfView')">商品棚</button>
+  </div>
+</template>
