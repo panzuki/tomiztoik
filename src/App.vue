29b7a28207b46db3eefbff838fbd8114389003fc
pan <77523808+panzuki@users.noreply.github.com>
2025-11-29 20:54:07 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,15 @@
 
 <script setup>
 import { ref, computed } from "vue";
+
+// components 配下を参照するように修正
 import TabBar from "./components/TabBar.vue";
-import ShelfCreate from "./views/ShelfCreate.vue";
-import Delivery from "./views/Delivery.vue";
-import ShelfView from "./views/ShelfView.vue";
+import ShelfCreate from "./components/ShelfCreate.vue";
+import Delivery from "./components/Delivery.vue";
+import ShelfView from "./components/ShelfView.vue";
 
 const currentTab = ref("shelfCreate");
+
 const currentTabComponent = computed(() => {
   if (currentTab.value === "shelfCreate") return ShelfCreate;
   if (currentTab.value === "delivery") return Delivery;
